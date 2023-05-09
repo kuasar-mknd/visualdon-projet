f8e35a93d39326cba2362828d55ad4c33bb46ecb
Samuel <samuel@mknd-records.com>
2023-05-09 21:05:21 +0200

    bug fixes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./style.css";
 import {graphTop10Country} from "./co2PerCapita.js";
 import {globe3d} from "./globe3d";
 
+let startY;
 
 document.getElementById("chart-container").addEventListener("click", function (event) {
     event.stopPropagation();
@@ -33,8 +34,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
     graphTop10Country();
 
-    window.addEventListener("wheel", function (event) {
-        if (event.deltaY > 0) {
+
+    function handleScroll(event, deltaY) {
+
+        // Hauteur totale du document
+        const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+
+        // Hauteur de la fenêtre
+        const windowHeight = window.innerHeight;
+
+        // Position actuelle du scroll
+        const currentScrollY = window.scrollY;
+
+        console.log(event.deltaY > 0 && currentScrollY + windowHeight >= documentHeight)
+        console.log(event.deltaY)
+        console.log(currentScrollY + windowHeight, documentHeight)
+
+        if (deltaY > 0 && Math.ceil(currentScrollY + windowHeight) >= documentHeight) {
             // Scrolling vers le bas
             description.style.opacity = 0;
             globeContainer.style.opacity = 0;
@@ -50,7 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 }, 500);
             }, 1000);
 
-        } else {
+        } else if (deltaY < 0 && currentScrollY === 0) {
             // Scrolling vers le haut
             secondChartContainer.style.opacity = 0;
             setTimeout(() => {
@@ -67,7 +83,24 @@ document.addEventListener("DOMContentLoaded", () => {
             }, 500);
 
         }
+    }
+
+    window.addEventListener("wheel", function (event) {
+        handleScroll(event, event.deltaY);
     });
 
+    window.addEventListener("touchstart", function (event) {
 
+        startY = event.touches[0].clientY;
+    });
+
+    window.addEventListener("touchmove", function (event) {
+        if (startY === null) {
+            return;
+        }
+
+        const deltaY = startY - event.touches[0].clientY;
+        handleScroll(event, deltaY);
+        startY = null;
+    });
 });
